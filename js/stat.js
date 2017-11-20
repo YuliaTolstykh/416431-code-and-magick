bd7c1941f9f1cfed843b05c98450f87212d0bbf5
YuliaTolstykh <yuliatolstykh@yandex.ru>
2017-11-20 22:41:25 +0300

    Переписаны функции

--- js/stat.js
+++ js/stat.js
@@ -1,43 +1,39 @@
 'use strict';
 
 window.renderStatistics = function (ctx, names, times) {
-  ctx.beginPath();
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.arc(130, 40, 20, 180 * (Math.PI / 180), 270 * (Math.PI / 180));
-  ctx.arc(510, 40, 20, 270 * (Math.PI / 180), 360 * (Math.PI / 180));
-  ctx.arc(510, 270, 20, 360 * (Math.PI / 180), 90 * (Math.PI / 180));
-  ctx.arc(130, 270, 20, 90 * (Math.PI / 180), 180 * (Math.PI / 180));
-  ctx.closePath();
-  ctx.fill();
-  ctx.stroke();
+  var roundCorners = function (color, x0, y0, radius, width, height) {
+    var x = [x0, x0 + width - 2 * radius, x0 + width - 2 * radius, x0];
+    var y = [y0, y0, y0 + height - 2 * radius, y0 + height - 2 * radius];
+    ctx.beginPath();
+    ctx.fillStyle = color;
+    for (var k = 0; k <= 3; k++) {
+      ctx.arc(x[k], y[k], radius, Math.PI * (1 + k / 2), Math.PI / 2 * (3 + k));
+    }
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  };
 
-  ctx.beginPath();
-  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
-  ctx.arc(120, 30, 20, 180 * (Math.PI / 180), 270 * (Math.PI / 180));
-  ctx.arc(500, 30, 20, 270 * (Math.PI / 180), 360 * (Math.PI / 180));
-  ctx.arc(500, 260, 20, 360 * (Math.PI / 180), 90 * (Math.PI / 180));
-  ctx.arc(120, 260, 20, 90 * (Math.PI / 180), 180 * (Math.PI / 180));
-  ctx.closePath();
-  ctx.fill();
-  ctx.stroke();
+  roundCorners('rgba(0, 0, 0, 0.7)', 130, 40, 20, 420, 270);
+  roundCorners('rgba(256, 256, 256, 1.0)', 120, 30, 20, 420, 270);
 
   ctx.fillStyle = '#000';
-  ctx.font = '14px PT Mono';
+  ctx.font = '16px PT Mono';
 
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
-
-  var max = -1;
-
-  for (var j = 0; j < times.length; j++) {
-    var time = times[j];
-    if (time > max) {
-      max = time;
+  var getMaxElement = function (array) {
+    var max = -1;
+    for (var j = 0; j < array.length; j++) {
+      var element = array[j];
+      if (element > max) {
+        max = element;
+      }
     }
-  }
-
+    return max;
+  };
   var histogramHeigth = 150; // px;
-  var step = histogramHeigth / (max - 0); // px;
+  var ratio = histogramHeigth / getMaxElement(times); // px;
   var initialX = 140; // px;
   var initialY = 250; // px;
   var barWidth = 40; // px;
@@ -51,9 +47,9 @@ window.renderStatistics = function (ctx, names, times) {
       var color = [0, 0, 255, parseFloat(Math.random().toFixed(1)) + 0.1];
       ctx.fillStyle = 'rgba' + '(' + color + ')';
     }
-    ctx.fillRect(initialX + indent * i, initialY, barWidth, -times[i] * step);
+    ctx.fillRect(initialX + indent * i, initialY, barWidth, -times[i] * ratio);
     ctx.fillStyle = '#000';
     ctx.fillText(names[i], initialX + indent * i, initialY + lineHeight);
-    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - times[i] * step - lineHeight / 2);
+    ctx.fillText(Math.round(times[i]), initialX + indent * i, initialY - times[i] * ratio - lineHeight / 2);
   }
 };
